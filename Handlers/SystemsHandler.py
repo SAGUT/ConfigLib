cae26a1baf1e98c1709d34d2231ad6ad9bcc7230
sascha.gutt@gmail.com <sascha.gutt@gmail.com>
2021-04-12 17:58:24 +0200

    Systemhandler added

---
+++ Handlers/SystemsHandler.py
@@ -0,0 +1,8 @@
+import logging
+from ..LibFrame import applicationDict
+from ..Objects.Project import Project
+class SystemHandler(object):
+
+    def check(self):
+        logging.debug("init")
+    
