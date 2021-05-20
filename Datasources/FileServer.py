dc12ff4eb245bbc81ec936a824337c81c24d4bba
sascha.gutt@gmail.com <sascha.gutt@gmail.com>
2021-05-20 23:42:44 +0200

    create subdirs

--- Datasources/FileServer.py
+++ Datasources/FileServer.py
@@ -1,4 +1,6 @@
 import logging
+import json
+import os
 from ..LibFrame import applicationDict
 from smb.SMBConnection import SMBConnection
 import socket
@@ -18,17 +20,57 @@ class FileServer(object):
 
     def initFileShare(self):
         try:
+            dire = os.path.dirname(__file__)
+            filename= os.path.join(dire, '../config/fileserver.json')
+            fp=open(filename,"r")
+            self.subdirs=json.load(fp)
+            fp.close()
+        except:
+            logging.error("could not load subdirs")
+        try:
             conn = self.getServerConnection()
             conn.connect(self.config['server_ip'],445)
-            sharedfiles = conn.listPath(self.config['sharename'], self.config['rootdir'])
-            for sharedfile in sharedfiles:
-                if sharedfile.isDirectory and not sharedfile.filename in [".",".."]:
-                    print(sharedfile.filename)
+            subdirs=["Projects"]
+            for subdir in subdirs:
+                newdir="{0}/{1}".format(self.config['rootdir'],subdir)
+                self.checkNCreate(conn,newdir)
+            conn.close()
+        except Exception as e:
+            logging.error(str(e))
+
+    def createNewProject(self,projectid):
+        try:
+            conn = self.getServerConnection()
+            conn.connect(self.config['server_ip'],445)
+            #conn.createDirectory(self.config['sharename'],"CMSData/Projects/{0}".format(projectid))
+            subdirs=["reports","configuration","data","data/scalar","data/twf","data/spectra","plots","plots/scalar","plots/nonscalar","reports/status"]
+            newdir="CMSData/Projects/{0}".format(projectid)
+            self.checkNCreate(conn,newdir)
+            for subdir in self.subdirs['subdirs']:
+                newdir="CMSData/Projects/{0}/{1}".format(projectid,subdir)
+                self.checkNCreate(conn,newdir)
             conn.close()
         except Exception as e:
             logging.error(str(e))
 
     
+    def checkNCreate(self,conn,sourcedir):
+        temp=str(sourcedir).split("/")
+        subdir=temp[-1]
+        temp.pop()
+        parentdir="/".join(temp)
+        print("checking: "+parentdir)
+        sharedfiles = conn.listPath(self.config['sharename'], parentdir)
+        makeit=True
+        for sharedfile in sharedfiles:
+            if sharedfile.isDirectory and sharedfile.filename==subdir:
+                print("found it")
+                makeit=False
+        if makeit:
+            newdir=parentdir+"/"+subdir
+            conn.createDirectory(self.config['sharename'],newdir)
+
+               
 
 
 
